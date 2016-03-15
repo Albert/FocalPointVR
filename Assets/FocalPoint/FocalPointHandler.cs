e02dc07ed94ef50106728ddae4aad839821d956b
Albert Hwang <hwang.al@gmail.com>
2016-03-15 00:50:12 -0400

    handles inertia w/out rigidbody

--- Assets/FocalPoint/FocalPointHandler.cs
+++ Assets/FocalPoint/FocalPointHandler.cs
@@ -8,8 +8,10 @@ public class FocalPointHandler : MonoBehaviour {
 	public bool translationInertia = false;
 	public bool rotationInertia = false;
 	public bool scaleInertia = false;
+	public float decayTime = 0.5f;
 
 	private Rigidbody rBody;
+	private bool wasKinematic;
 	private bool isActivated;
 	private bool lastIsActivated;
 	private Vector3 lastPosition;
@@ -18,9 +20,13 @@ public class FocalPointHandler : MonoBehaviour {
 	private Vector3 penultimateLookAt;
 	private Vector3 lastScale;
 	private Vector3 penultimateScale;
+	private float timeOfRelease = 0.0f;
 
 	void Start () {
 		rBody = GetComponent<Rigidbody> ();
+		if (rBody != null) {
+			wasKinematic = rBody.isKinematic;
+		}
 	}
 
 	void Update () {
@@ -35,25 +41,50 @@ public class FocalPointHandler : MonoBehaviour {
 			}
 			if (scaleInertia) {
 				penultimateScale = lastScale;
-				lastScale = transform.localScale;
+				lastScale = transform.lossyScale;
+			}
+		}
+		// TODO: clean up all these if statements
+		if (lastIsActivated != isActivated) {
+			if (rBody != null && !wasKinematic) {
+				rBody.isKinematic = isActivated;
+				if (!isActivated) {
+					if (translationInertia) {
+						rBody.velocity = (lastPosition - penultimatePosition) / Time.deltaTime;
+					}
+					if (rotationInertia) {
+//						http://answers.unity3d.com/questions/48836/determining-the-torque-needed-to-rotate-an-object.html
+						Vector3 x = Vector3.Cross (penultimateLookAt, lastLookAt);
+						float theta = Mathf.Asin (x.magnitude);
+						Vector3 w = x.normalized * theta / Time.fixedDeltaTime;
+						Quaternion q = transform.rotation * rBody.inertiaTensorRotation;
+						Vector3 T = q * Vector3.Scale (rBody.inertiaTensor, (Quaternion.Inverse (q) * w));
+						rBody.AddTorque (T, ForceMode.Impulse);
+					}
+				}
+			} else {
+				if (!isActivated) {
+					timeOfRelease = Time.time;
+				}
 			}
 		}
 
-		if (isActivated != lastIsActivated) {
-			rBody.isKinematic = isActivated; // TODO this should be sensitive to rBody.isKinematic's existence
-			if (!isActivated) {
-				if (translationInertia) {
-					rBody.velocity = (lastPosition - penultimatePosition) / Time.deltaTime;
-				}
-				if (rotationInertia) {
-//					http://answers.unity3d.com/questions/48836/determining-the-torque-needed-to-rotate-an-object.html
-					Vector3 x = Vector3.Cross (penultimateLookAt, lastLookAt);
-					float theta = Mathf.Asin (x.magnitude);
-					Vector3 w = x.normalized * theta / Time.fixedDeltaTime;
-					Quaternion q = transform.rotation * rBody.inertiaTensorRotation;
-					Vector3 T = q * Vector3.Scale (rBody.inertiaTensor, (Quaternion.Inverse (q) * w));
-					rBody.AddTorque (T, ForceMode.Impulse);
-				}
+		if (!isActivated && decayTime > (Time.time - timeOfRelease)) {
+			float lerpT = (decayTime - (Time.time - timeOfRelease)) / decayTime;
+			if (translationInertia) {
+				Vector3 full = lastPosition - penultimatePosition;
+				Vector3 lerpPartial = Vector3.Lerp (Vector3.zero, full, lerpT);
+				transform.position += lerpPartial;
+			}
+			if (rotationInertia) {
+				Quaternion full = Quaternion.FromToRotation (transform.InverseTransformDirection (penultimateLookAt), transform.InverseTransformDirection (lastLookAt));
+				Quaternion lerpPartial = Quaternion.Slerp (Quaternion.identity, full, (decayTime - (Time.time - timeOfRelease)) / decayTime);
+				transform.rotation *= lerpPartial;
+			}
+			if (scaleInertia) {
+				Vector3 full = (lastScale - penultimateScale);
+				Vector3 lerpPartial = Vector3.Lerp (Vector3.zero, full, lerpT);
+				transform.localScale += lerpPartial;
 			}
 		}
 
